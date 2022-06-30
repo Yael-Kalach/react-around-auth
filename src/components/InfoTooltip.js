5d5c560a845379c6dd9a69d3c38f72d4e230c931
Yael Kalach <ylklk13579@gmail.com>
2022-06-30 18:14:54 +0300

    moving computers

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,16 +1,11 @@
-function InfoToolTip(props) {
-    const overlayName = `popup__overlay info-popup__overlay`
-    const containerName = `popup__container info-popup__container`
-    const closeButtonName = `popup__close-button info-popup__close-button`
-
-  
+function InfoToolTip( isOpen, onClose) {
     return (
-      <div className= {`popup info-popup ${props.isOpen ? 'popup_visible' : ''}`}>
-        <div className={overlayName} onClick={props.onClose}></div>
-        <div className={containerName}>
-          <button type="button" aria-label="close" className={closeButtonName} onClick={props.onClose}></button>
-          <img className={`info-popup__image ${props.isSuccessful ? 'info-popup__image_success' : 'info-popup__image_error'}`} />
-          <p className="info-popup__caption">{props.isSuccessful ? `Success! You have now been registered.` : `Oops, something went wrong! Please try again.`}</p>
+      <div className= {`popup info-popup ${isOpen ? 'popup_visible' : ''}`}>
+        <div className="popup__overlay" onClick={onClose}></div>
+        <div className="popup__container">
+          <button type="button" aria-label="close" className="popup__close-button" onClick={onClose}></button>
+          <img className={`info-popup__image ${isSuccessful ? 'info-popup__image_success' : 'info-popup__image_error'}`} />
+          <p className="info-popup__caption">{isSuccessful ? `Success! You have now been registered.` : `Oops, something went wrong! Please try again.`}</p>
         </div>
       </div>
     ) 
